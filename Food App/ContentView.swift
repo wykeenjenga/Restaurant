0d452c1d2a758536e2321b12665650bbf758dc19
wykee2 <46722362+wykeenjenga@users.noreply.github.com>
2022-05-03 17:03:46 +0300

    Initial Commit

---
+++ Food App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Food App
+//
+//  Created by Wykee Njenga on 5/3/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Food App/Food_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Food_AppApp.swift
+//  Food App
+//
+//  Created by Wykee Njenga on 5/3/22.
+//
+
+import SwiftUI
+
+@main
+struct Food_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
